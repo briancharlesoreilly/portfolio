ef1a5a314a3196d486247493925cd721d920f141
briancharlesoreilly <briancharlesoreilly@gmail.com>
2022-04-25 18:45:51 -0400

    add mobile burger menu and javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-console.log("what it do");
+// declare variables
+const burgerMenu = document.querySelector(".burger-slide-out");
+const burgerCheckbox = document.querySelector("#burger-menu");
+
+// event listener for when user clicks anywhere int he menu other than a link
+burgerMenu.addEventListener("click", menuSlide);
+
+// close slide out menu when user toggles the checkbox
+function menuSlide() {
+  if (burgerCheckbox.checked === true) {
+    burgerCheckbox.checked = false;
+  } else {
+    burgerCheckbox.checked = true;
+  }
+}
+
