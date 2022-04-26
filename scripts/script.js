6092163f2339786ca98d29e89104fd95a808961b
briancharlesoreilly <briancharlesoreilly@gmail.com>
2022-04-26 09:41:28 -0400

    final touches

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
+// **********************
+// burger menu javascript
+// **********************
+
 // declare variables
 const burgerMenu = document.querySelector(".burger-slide-out");
 const burgerCheckbox = document.querySelector("#burger-menu");
 
-// event listener for when user clicks anywhere int he menu other than a link
+// event listener for when user clicks anywhere in the menu other than a link
 burgerMenu.addEventListener("click", menuSlide);
 
 // close slide out menu when user toggles the checkbox
@@ -13,4 +17,39 @@ function menuSlide() {
     burgerCheckbox.checked = true;
   }
 }
+
+// *************************
+// formspree form javascript
+// *************************
+const form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById("my-form-status");
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      "Accept": "application/json"
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, "errors")) {
+          status.innerHTML = data["errors"].map(error => error["message"])
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form."
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form."
+  });
+}
+form.addEventListener("submit", handleSubmit)
+
 
